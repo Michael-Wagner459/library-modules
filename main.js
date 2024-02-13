1a68d4e42971dc69fa95c421d42ca60685e8e384
michael-wagner459 <wagner.michael4592@gmail.com>
2024-02-13 15:27:37 -0800

    fixed all errors in practice example

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
 // write your code here to make the tests pass
+var Library = function () {
+  var books = [];
+
+  var addBook = function (book) {
+    books.push(book);
+  }
+
+  var checkOutBook = function (book) {
+    if (books.includes(book)) {
+     book.setAttribute('checkedOut', true)
+    } 
+    else {
+      console.log('Book is not in the Library')
+    }
+  };
+
+  var returnBook = function (book) {
+    if (books.includes(book)) {
+    book.setAttribute("checkedOut", false)
+    }
+    else {
+      console.log("Book is not in the Library.")
+    }
+  }
+  return {
+    addBook: addBook,
+    checkOutBook: checkOutBook,
+    returnBook: returnBook
+  };
+};
+
+var Book = function (title, author) {
+
+  var attributes = {
+    checkedOut: false,
+    title: title,
+    author: author
+  } 
+
+  var getAttribute = function (prop) {
+    if (attributes.hasOwnProperty(prop)) {
+    return attributes[prop]
+    }
+  }
+
+  var setAttribute = function (prop, value) {
+    if (attributes.hasOwnProperty(prop)) {
+    return attributes[prop] = value;
+    }
+  }
+
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  }
+};
+
